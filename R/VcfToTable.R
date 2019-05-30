9333b858d2e2959f26c8a79c33443139f91938cc
Laura Morales <50193789+lauramoralesphd@users.noreply.github.com>
2019-05-30 13:45:19 +0200

    Update VcfToTable.R

--- R/VcfToTable.R
+++ R/VcfToTable.R
@@ -1,13 +1,13 @@
-  #' Converts VCF file to data frame.
-  #'
-  #' @param vcf.file String specifying name of VCF file in .vcf or .vcf.gz format.
-  #' @return Data frame containing data from VCF file.
-  #' @examples
-  #' vcf.table=VcfToTable("fileName.vcf")
-  #' @export
-  #' VcfToTable()
-  VcfToTable <- function(vcf.file){
-    vcf <- read.vcfR(vcf.file, verbose=F, convertNA=F)
-    vcf.table <- data.frame(vcf@fix, vcf@gt, stringsAsFactors=F)
-    return(vcf.table)
+#' Converts VCF file to data frame.
+#'
+#' @param vcf.file String specifying name of VCF file in .vcf or .vcf.gz format.
+#' @return Data frame containing data from VCF file.
+#' @examples
+#' vcf.table=VcfToTable("fileName.vcf")
+#' @export
+#' VcfToTable()
+VcfToTable <- function(vcf.file){
+  vcf <- read.vcfR(vcf.file, verbose=F, convertNA=F)
+  vcf.table <- data.frame(vcf@fix, vcf@gt, stringsAsFactors=F)
+  return(vcf.table)
 }
